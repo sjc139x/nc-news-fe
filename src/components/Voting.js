56281179af8952761387d7f64cc4671436a94694
Samantha Cockerill <samanthajaynecockerill@gmail.com>
2019-05-20 12:19:23 +0100

    updating voting component so that you can vote a maximum of once in EITHER direction

--- src/components/Voting.js
+++ src/components/Voting.js
@@ -4,17 +4,19 @@ import axios from 'axios';
 class Voting extends React.Component {
     state = {
         voteModifier: 0,
-        areButtonsDisabled: false
+        areButtonsDisabled: false,
+        isUpvoteButtonDisabled: false,
+        isDownvoteButtonDisabled: false
     }
 
     render () {
         const { loggedInUser, votes, type } = this.props;
         let { voteModifier } = this.state;
-        const { areButtonsDisabled } = this.state;
+        const { isUpvoteButtonDisabled, isDownvoteButtonDisabled } = this.state;
         voteModifier += votes;
         return (
             <div className="votingTile">
-                {loggedInUser && <button type="button" disabled={areButtonsDisabled} onClick={e => this.changeVotes(1)} className="votingButtons" >YUM</button>}
+                {loggedInUser && <button type="button" disabled={isUpvoteButtonDisabled} value="yum" onClick={e => this.changeVotes(1, e.target.value)} className="votingButtons" >YUM</button>}
 
                 {(voteModifier === 0) && <h5 id="voteValue">• no votes •</h5>}
                 {(voteModifier === 1) && <h5 id="voteValue">• {voteModifier} person liked this {type.slice(0, -1)} •</h5>}
@@ -22,12 +24,12 @@ class Voting extends React.Component {
                 {(voteModifier > 1) && <h5 id="voteValue">• {voteModifier} people liked this {type.slice(0, -1)} •</h5>}
                 {(voteModifier < -1) && <h5 id="voteValue">• {Math.abs(voteModifier)} people disliked this {type.slice(0, -1)} •</h5>}
                 
-                {loggedInUser && <button type="button" disabled={areButtonsDisabled} onClick={e => this.changeVotes(-1)} className="votingButtons" >YUCK</button>}
+                {loggedInUser && <button type="button" disabled={isDownvoteButtonDisabled} value="yuck" onClick={e => this.changeVotes(-1, e.target.value)} className="votingButtons" >YUCK</button>}
             </div>
         )
     }
 
-    changeVotes = (integer) => {
+    changeVotes = (integer, direction) => {
         const { type, id } = this.props;
 
         axios.patch(`https://nc-news-sjc.herokuapp.com/api/${type}/${id}`, {
@@ -38,7 +40,13 @@ class Voting extends React.Component {
             voteModifier: prevState.voteModifier + integer
         }));
 
-        this.setState({ areButtonsDisabled: true });
+        if (direction === "yum") {
+            this.setState({ isUpvoteButtonDisabled: true });
+            this.setState({ isDownvoteButtonDisabled: false });
+        } else if (direction === "yuck") {
+            this.setState({ isDownvoteButtonDisabled: true });
+            this.setState({ isUpvoteButtonDisabled: false });
+        }
     }
 }
 
