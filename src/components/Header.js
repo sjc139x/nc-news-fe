99ca497fef1a955a4acceb7a6380f2177afd3fa6
Samantha Cockerill <samanthajaynecockerill@gmail.com>
2019-05-20 16:59:40 +0100

    improving responsiveness of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import { Link } from '@reach/router';
-import LoggedIn from './LoggedInTile';
+import LoggedInTile from './LoggedInTile';
 
 function Header ({ toggleLogInBox, toggleSignUpBox, loggedInUser, logOutUser }) {
     return (
         <div>
-            <img src="https://i.ibb.co/r3dSYj6/Screen-Shot-2019-05-13-at-09-32-38.png" alt="logo" id="logo"/>
+            <img src="https://i.ibb.co/r3dSYj6/Screen-Shot-2019-05-13-at-09-32-38.png" alt="logo" className="Header-Logo"/>
                 {!loggedInUser && (
-                    <div id="userAccountButtons">
+                    <div className="Header-UserAccountButtons">
                         <button type="button" onClick={toggleLogInBox}>LOG IN</button>
-                        <br/>
                         <br/>
                         <button type="button" onClick={toggleSignUpBox}>SIGN UP</button>
                     </div>
                 )}
                 {loggedInUser && (
                     <div>
-                        <LoggedIn userInfo={loggedInUser} logOutUser={logOutUser}/>
+                        <LoggedInTile userInfo={loggedInUser} logOutUser={logOutUser}/>
                     </div>
                 )}
-            <ul className="navBar">
-                <li id="navBarListItem" key="home"><Link to="/">home</Link></li>
-                <li id="navBarListItem" key="recipes"><Link to="/recipes">recipes</Link></li>
-                <li id="navBarListItem" key="reviews"><Link to="/reviews">reviews</Link></li>
-                <li id="navBarListItem" key="top5"><Link to="/top5">top 5</Link></li>
-                <li id="navBarListItem" key="chef"><Link to="/chef">chef's table</Link></li>
-                <li id="navBarListItem" key="healthy"><Link to="/healthy">healthy living</Link></li>
-                <li id="navBarListItem" key="treats"><Link to="/treats">treat yo' self</Link></li>
+            <ul className="Header-NavBar">
+                <li className="Header-NavBarListItems" key="home"><Link to="/">home</Link></li>
+                <li className="Header-NavBarListItems" key="recipes"><Link to="/recipes">recipes</Link></li>
+                <li className="Header-NavBarListItems" key="reviews"><Link to="/reviews">reviews</Link></li>
+                <li className="Header-NavBarListItems" key="top5"><Link to="/top5">top 5</Link></li>
+                <li className="Header-NavBarListItems" key="chef"><Link to="/chef">chef's table</Link></li>
+                <li className="Header-NavBarListItems" key="healthy"><Link to="/healthy">healthy living</Link></li>
+                <li className="Header-NavBarListItems" key="treats"><Link to="/treats">treat yo' self</Link></li>
             </ul>
         </div>
     )
--- src/components/LoggedInTile.js
+++ src/components/LoggedInTile.js
@@ -4,10 +4,10 @@ import { Link } from '@reach/router';
 function LoggedInTile (props) {
     const { userInfo, logOutUser } = props;
     return (
-        <div id="loggedInInfoBox">
-            <img src={userInfo.avatar_url} alt="avatar" className="loggedInUserImage"/>
-            <p id="welcomeBackText">Welcome back, <Link to={`/profile/${userInfo.username}`}>{userInfo.username}</Link>!</p>
-            <button type="button" onClick={logOutUser}id="signOutButton">LOG OUT</button>
+        <div className="LoggedInTile-Container">
+            <img src={userInfo.avatar_url} alt="avatar" className="LoggedInTile-AvatarImage"/>
+            <p className="LoggedInTile-WelcomeBackText">Welcome back, <Link to={`/profile/${userInfo.username}`}>{userInfo.username}</Link>!</p>
+            <button type="button" onClick={logOutUser} className="LoggedInTile-LogOutButton">LOG OUT</button>
         </div>
     )
 };
